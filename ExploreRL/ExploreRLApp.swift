36e418b4b215897d81eaa3af2f8b9498dc5dcfbd
justindal <justin_daludado10@icloud.com>
2025-11-16 15:25:03 -0500

    Initial Commit

---
+++ ExploreRL/ExploreRLApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreRLApp.swift
+//  ExploreRL
+//
+//  Created by Justin Daludado on 2025-11-16.
+//
+
+import SwiftUI
+
+@main
+struct ExploreRLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
